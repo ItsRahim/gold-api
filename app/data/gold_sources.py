ec371a52ceb1660b1e79b68b4e72cb4b28b81db9
ItsRahim <rahim1605@gmail.com>
2023-10-20 23:10:08 +0100

    added method to find if requested source exists

--- app/data/gold_sources.py
+++ app/data/gold_sources.py
@@ -1,3 +1,7 @@
+from app.config.logging_config import configure_logging
+
+log = configure_logging()
+
 """
 This is a Python file used to store website information.
 
@@ -16,6 +20,7 @@ current XAU/GBP price information.
 """
 UK_INVESTING_DICT = {
     "name": "UK Investing",
+    "request_name": "uk_investing",
     "url": "https://uk.investing.com/currencies/xau-gbp",
     "element": ["span", {"class": "text-2xl", "data-test": "instrument-price-last"}]
 }
@@ -23,45 +28,58 @@ UK_INVESTING_DICT = {
 # TODO: Change implementation - DO NOT USE
 UNUSED_TRADING_VIEW_DICT = {
     "name": "Trading View",
+    "request_name": "trading_view",
     "url": "https://www.tradingview.com/symbols/XAUGBP/",
     "element": ["span", {"class": "last-JWoJqCpY js-symbol-last"}]
 }
 
-
 # TODO: Fix the element to retrieve price - DO NOT USE
 UNUSED_BLOOMBERG_DICT = {
     "name": "Bloomberg",
+    "request_name": "bloomberg",
     "url": "https://www.bloomberg.com/quote/XAUGBP:CUR",
     "element": ["div", {'data-component': 'sized-price', 'class': 'sized-price SizedPrice_extraLarge-05pKbJRbUH8-'}]
 }
 
-
 CNBC_DICT = {
     "name": "CNBC",
+    "request_name": "cnbc",
     "url": "https://www.cnbc.com/quotes/XAUGBP=",
     "element": ["span", {'class': 'QuoteStrip-lastPrice'}]
 }
 
-
 FORBES_DICT = {
     "name": "Forbes",
+    "request_name": "forbes",
     "url": "https://www.forbes.com/advisor/money-transfer/currency-converter/xau-gbp/",
     "element": ["span", {'class': 'amount'}]
 }
 
-
 # TODO: Fake user agent required
 GOLD_UK_DICT = {
-        "name": "Gold UK",
-        "url": "https://www.cnbc.com/quotes/XAUGBP=",
-        "element": ["span", {'class': 'QuoteStrip-lastPrice'}]
+    "name": "Gold UK",
+    "request_name": "gold_uk",
+    "url": "https://www.cnbc.com/quotes/XAUGBP=",
+    "element": ["span", {'class': 'QuoteStrip-lastPrice'}]
 }
-
 
 # TODO: Fake user agent required
 BULLION_BY_POST_DICT = {
     "name": "BullionByPost",
+    "request_name": "bullion_by_post",
     "url": "https://www.bullionbypost.co.uk/gold-price/",
     "element": ["span", {'name': 'current_price_field', 'data-currency': 'default'}]
 }
 
+
+def get_source(requested_source: str):
+    sources = [UK_INVESTING_DICT, UNUSED_TRADING_VIEW_DICT, UNUSED_BLOOMBERG_DICT, CNBC_DICT, FORBES_DICT,
+               GOLD_UK_DICT, BULLION_BY_POST_DICT]
+    try:
+        for source in sources:
+            if source.get("request_name") == requested_source.lower():
+                return source
+        return None
+    except Exception as e:
+        log.error(f"An error occurred while searching for the dictionary: {e}")
+        return None
