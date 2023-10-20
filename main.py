b567b9f24b6d9774fc74fa32db334e42e3ddc455
ItsRahim <rahim1605@gmail.com>
2023-10-20 20:16:06 +0100

    Added handles to erroneous endpoints

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
+import uvicorn
+from fastapi import FastAPI, HTTPException
+from starlette.routing import Route
+from starlette.requests import Request
+from app.api.endpoints import price_router
+
+app = FastAPI()
+app.include_router(price_router, prefix="/api/v1/gold")
+
+
+async def catch_all(request: Request):
+    raise HTTPException(status_code=404, detail=f'Endpoint {request.method} {request.url.path} not found')
+
+catch_all_route = Route("/{path:path}", catch_all, methods=["GET", "POST", "PUT", "DELETE"])
+app.router.routes.append(catch_all_route)
+
+
 if __name__ == "__main__":
-    print("HELLO")
+    uvicorn.run(app, host="localhost", port=5000)
