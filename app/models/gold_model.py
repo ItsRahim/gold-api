727a22ae8ab7b83b85fd0f30546578d7843a0612
ItsRahim <rahim1605@gmail.com>
2023-10-20 23:10:20 +0100

    created model for gold

--- app/models/gold_model.py
+++ app/models/gold_model.py
@@ -1,13 +1,32 @@
 from dataclasses import dataclass
 from datetime import datetime
+from app.config.logging_config import configure_logging
+
+
+log = configure_logging()
 
 
 @dataclass
 class Gold:
     price: float
-    date_time: datetime
+    date: datetime
     source: str
 
-    @property
-    def formatted_datetime(self):
-        return self.date_time.strftime("%Y-%m-%d %H:%M:%S")
+    def __post_init__(self):
+        self.format_datetime()
+        self.format_price()
+
+    def format_datetime(self):
+        try:
+            formatted_date = datetime.strptime(str(self.date), "%Y-%m-%d %H:%M:%S")
+            return formatted_date
+        except ValueError:
+            log.error(f"Error formatting date for Gold object: {self}")
+            return None
+
+    def format_price(self):
+        try:
+            self.price = round(float(self.price), 2)
+        except ValueError:
+            self.price = None
+            log.error(f"Error formatting price for Gold object: {self}")
