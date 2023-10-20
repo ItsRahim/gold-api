54e1553215f0140423dae38ab901cc7d713f5160
ItsRahim <rahim1605@gmail.com>
2023-10-20 23:40:37 +0100

    changed host and port to be in env variable

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
+import os
+
 import uvicorn
 from fastapi import FastAPI, HTTPException
 from starlette.routing import Route
 from starlette.requests import Request
 from app.api.endpoints import price_router
+from dotenv import load_dotenv
 
 app = FastAPI()
 app.include_router(price_router, prefix="/api/v1/gold")
+load_dotenv()
 
 
 async def catch_all(request: Request):
@@ -16,4 +20,7 @@ app.router.routes.append(catch_all_route)
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=5000)
+    host = os.environ.get("FAST_API_HOST")
+    port = int(os.environ.get("FAST_API_PORT"))
+
+    uvicorn.run(app, host=host, port=port)
