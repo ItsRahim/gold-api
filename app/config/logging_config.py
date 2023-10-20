a9aef03989b332315dca18245436334921b5e27f
ItsRahim <rahim1605@gmail.com>
2023-10-20 23:09:48 +0100

    removed logging to console

--- app/config/logging_config.py
+++ app/config/logging_config.py
@@ -15,16 +15,11 @@ def configure_logging():
     file_handler = logging.FileHandler(LOG_FILE)
     file_handler.setLevel(logging.INFO)
 
-    console_handler = logging.StreamHandler()
-    console_handler.setLevel(logging.DEBUG)
-
     formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
     file_handler.setFormatter(formatter)
-    console_handler.setFormatter(formatter)
 
     logger.addHandler(file_handler)
-    logger.addHandler(console_handler)
 
     return logger
 
