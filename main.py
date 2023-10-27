4e07a7dcdc5db6ef5c7b47c47e5a366ca7b7cdb5
ItsRahim <rahim1605@gmail.com>
2023-10-27 18:27:50 +0100

    reverted changes

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from starlette.routing import Route
 from starlette.requests import Request
 from app.api.endpoints import price_router
 from app.config.load_config import load_config
-from app.kafka.producer import initialise
 
 app = FastAPI()
 app.include_router(price_router, prefix="/api/v1/gold")
@@ -20,7 +19,6 @@ app.router.routes.append(catch_all_route)
 
 
 if __name__ == "__main__":
-    initialise()
     host = config['host']
     port = config['port']
 
